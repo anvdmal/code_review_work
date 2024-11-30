2c4f3bcb9813bf962620b9957a6349c4b01fcc8e
anvdmal <alekseeva.ann04@mail.ru>
2024-11-30 12:44:05 +0300

    add calculator methods unit-tests

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -3,29 +3,31 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+    Calculator calculator = new Calculator();
 
     @Test
     void add() {
-        //TODO inser your realisation in test-method add
+        assertEquals(10, calculator.add(8, 2));
     }
 
     @Test
     void dif() {
-        //TODO inser your realisation in test-method dif
+        assertEquals(6, calculator.dif(8, 2));
     }
 
     @Test
     void div() {
-        //TODO inser your realisation in test-method div
+        assertEquals(4, calculator.div(8, 2));
+        assertThrows(IllegalArgumentException.class, () -> calculator.div(8, 0));
     }
 
     @Test
     void times() {
-        //TODO inser your realisation in test-method times
+        assertEquals(16, calculator.times(8, 2));
     }
 
     @Test
     void solver() {
-        //TODO inser your realisation in test-method solver
+        assertEquals(132, calculator.solver(8, 2, 3));
     }
 }
